904a5ad182ee7b1e230ad9938c6bbe72f9af5e56
Sanchous <sanchous1981@yandex.ru>
2021-08-23 20:42:55 +0400

    NEW #1

---
+++ project cataclysmius.cpp
@@ -0,0 +1,40 @@
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <ctime>
+using namespace std;
+//class Point {
+//	int X{ 0 };
+//	int Y{ 0 };
+//public:
+//	Point() :X{ 0 }, Y{ 0 }{};
+//	Point(int x, int y) :X{ x }, Y{ y } {};
+//	friend ostream& operator<<(ostream os, Point p) {};
+//	Point operator*(const Point& p1) {
+//		return Point(p1.X * X, p1.Y * Y);
+//		Point& operator(int) {
+//			X++; Y++;
+//			return *this;
+//		}
+//		Point& operator++() {
+//			Point p{ X,Y };
+//			X++;Y++;
+//			return p;
+//	}
+//};
+//friend ostream& operator<<(ostream& os, Point p) {
+//	return os<< "x=" << p.X << ", y=" << p.Y;
+//}
+int main() {
+//
+//	Point p1{ 2,7 }, p2{ 9,12 }, p3;
+//	cout << p1 << endl;
+//	cout << p2 << endl;
+//	p3 = p1 + p2;
+//	p3++;
+//	cout << p3++ << endl;
+//	cout << p3 << endl;
+
+}
